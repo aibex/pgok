57082b4fc0aaa7b625b5d8fe6d4591ee6ee0d55e
Jakob Heuser <jakob@felocity.com>
2019-08-08 17:07:05 -0700

    new migration system, pgok library, converted to official apollo react hooks
    Former-commit-id: 594e2009ae266549c7a8ab0b0aa765cd325d8416

---
+++ esm/cli.js
@@ -0,0 +1,121 @@
+import exec from "./index";
+
+const getArgs = require("command-line-args");
+const getUsage = require("command-line-usage");
+const DEFAULT_BACKOFF = 1.4;
+
+const optionDefinitions = [
+  {
+    name: "help",
+    alias: "h",
+    type: Boolean,
+    description: "Print this usage guide",
+  },
+  {
+    name: "verbose",
+    alias: "v",
+    type: Boolean,
+    description: "Print detailed log information while running this command",
+  },
+  {
+    name: "retries",
+    alias: "r",
+    type: Boolean,
+    description:
+      "The number of tries to make before giving up with exit code `1` (defaults to `10`)",
+  },
+  {
+    name: "time",
+    alias: "t",
+    type: Boolean,
+    description:
+      "The initial amount of time before each retry in ms (defaults to `100`)",
+  },
+  {
+    name: "backoff",
+    alias: "b",
+    type: Boolean,
+    description: `The backoff factor to use when increasing time between retries. Values less than 1 are set to the default. (defaults to \`${DEFAULT_BACKOFF}\`)`,
+  },
+  {
+    name: "quiet",
+    alias: "q",
+    type: Boolean,
+    description: "Overrides --verbose -v. Silences all output.",
+  },
+  {
+    name: "uri",
+    type: String,
+    typeLabel: "{underline string}",
+    defaultOption: true,
+    defaultValue: "postgres://postgres:postgres@localhost:5432/public",
+    description:
+      "A fully formed postgres connection string you want to ensure is accessible",
+  },
+  {
+    name: "env",
+    alias: "e",
+    type: String,
+    typeLabel: "{underline string}",
+    description: "An optional ENV to reference for the connection uri.",
+  },
+];
+
+const usageDoc = [
+  {
+    header: "pgok - Wait for a postgres instance",
+    content: "A simple blocking CLI that can ensure you don't query() early",
+  },
+  {
+    header: "Usage",
+    content: ["$ pgok [options] URI", "$ pgok -e ENV_VAR [options]"],
+  },
+  {
+    header: "Specify a URI",
+    content: [
+      "pgok can be launched wither with a URI, or by specifying the -e flag. Specifying -e will cause pgok to look for the Postgres Connection String at the URL contained within process.env[ENV_NAME]",
+    ],
+  },
+  {
+    header: "Options",
+    optionList: optionDefinitions,
+  },
+];
+
+const cli = () => {
+  const usage = getUsage(usageDoc);
+  let args;
+
+  try {
+    args = getArgs(optionDefinitions);
+  } catch (e) {
+    console.error(e.message);
+    console.log(usage);
+    process.exit(1);
+  }
+
+  const connectionString = args.env ? process.env[args.env] : args.uri;
+  console.log(args.env);
+  if (args.help) {
+    console.log(usage);
+    process.exit(0);
+  }
+
+  const backoff =
+    typeof args.backoff === "undefined"
+      ? DEFAULT_BACKOFF
+      : parseFloat(args.backoff);
+
+  const opts = {
+    uri: connectionString,
+    verbose: args.verbose,
+    quiet: args.quiet,
+    retries: typeof args.retries === "undefined" ? 10 : parseInt(args.retries),
+    time: typeof args.time === "undefined" ? 100 : parseInt(args.time),
+    backoff: backoff > 1 ? backoff : DEFAULT_BACKOFF,
+  };
+
+  exec(opts);
+};
+
+export default cli;
---
+++ esm/index.js
@@ -0,0 +1,46 @@
+const { Client } = require("pg");
+const DEFAULT_BACKOFF = 1.4;
+
+const exec = opts => {
+  const backoff = opts.backoff <= 1 ? DEFAULT_BACKOFF : opts.backoff;
+  const verbose = opts.quiet ? 0 : opts.verbose ? 2 : 1;
+  let pause = opts.time;
+  let attempts = opts.retries;
+
+  const info = msg => (verbose > 1 ? console.info(msg) : null);
+  const log = msg => (verbose > 0 ? console.log(msg) : null);
+  const error = msg => (verbose > 0 ? console.error(msg) : null);
+
+  const schedule = () => {
+    attempts--;
+    pause = Math.floor(pause * backoff);
+    if (attempts <= 0) {
+      error("Could not connect to postgres");
+      process.exit(1);
+    }
+    info(
+      `Retrying Connection in ${pause}ms (attempt ${opts.retries - attempts})`
+    );
+    return setTimeout(() => pgwait(), pause);
+  };
+
+  const pgwait = () => {
+    const client = new Client({ connectionString: opts.uri });
+    client.connect(err => {
+      if (err) {
+        info("Connection Error:");
+        info(err);
+        return schedule();
+      }
+
+      log("Postgres Connection OK");
+      process.exit(0);
+    });
+  };
+
+  log("Connecting to Postgres");
+  info(opts.connectionString);
+  pgwait();
+};
+
+export default exec;
